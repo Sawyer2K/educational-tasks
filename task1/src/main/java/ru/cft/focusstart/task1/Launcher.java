1823e404d9efd5c017a596853e4617a15e0d11eb
Andrey Sbitnev <sbitnev.job@yandex.ru>
2021-10-21 13:02:14 +0700

    minor changes in Launcher class

--- task1/src/main/java/ru/cft/focusstart/task1/Launcher.java
+++ task1/src/main/java/ru/cft/focusstart/task1/Launcher.java
@@ -9,23 +9,23 @@ public class Launcher {
     }
 
     public static void run() {
-        DataHolder dataHolder;
+        MatrixGenerator matrixGeneratorImpl;
         Drawer drawer;
 
-        System.out.println("Введите размерность таблицы умножения. Допустимы размерности от 1 до 32.\n" +
+        System.out.println("Введите размерность таблицы умножения. Допустимы размерности от 1 до 32 включительно.\n" +
                 "Ввод 0 приведёт к закрытию программы");
 
         int input = processUserInput();
 
         if (input == 0) {
-            System.out.println("Программа завершена коректно");
+            System.out.println("Программа завершена корректно");
             System.exit(0);
         }
 
-        dataHolder = new DataHolder(input);
-        drawer = new Drawer(input);
+        matrixGeneratorImpl = new MatrixGeneratorImpl(input);
+        drawer = new Drawer();
 
-        drawer.outputTheTable(dataHolder);
+        drawer.outputTheTable(matrixGeneratorImpl);
     }
 
     public static int processUserInput() {
@@ -35,7 +35,14 @@ public class Launcher {
             String input = scanner.next();
 
             if (input.matches("\\d+")) {
-                return Integer.parseInt(input);
+                int result = Integer.parseInt(input);
+
+                if (result <= 32) {
+                    return result;
+                } else {
+                    System.out.println("Введённое число должно быть в промежутке от 0 до 32 включительно.\n" +
+                            "Повторите ввод.");
+                }
             } else {
                 System.out.println("Входные данные некорректны. Повторите ввод.");
             }
