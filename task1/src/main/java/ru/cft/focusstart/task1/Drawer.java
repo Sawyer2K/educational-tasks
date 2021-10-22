013ecc401d2f450b2d81d44e2b34d88020153a25
Andrey Sbitnev <sbitnev.job@yandex.ru>
2021-10-22 18:45:00 +0700

    Bulk refactoring of the entire class. Renaming and consolidation getColumnUnderline and getColumnWidth methods

--- task1/src/main/java/ru/cft/focusstart/task1/Drawer.java
+++ task1/src/main/java/ru/cft/focusstart/task1/Drawer.java
@@ -2,116 +2,103 @@ package ru.cft.focusstart.task1;
 
 public class Drawer {
 
-    private final String PIECE = "-";
-    private final String VERTICAL_SEPARATOR = "|";
-    private final String KNUCKLE = "+";
-    private String firstColumn;
-    private String standardColumn;
-    private String horizontalSeparator;
+    private final String HORIZONTAL_BORDER = "-";
+    private final String VERTICAL_BORDER = "|";
+    private final String CROSS_BORDER = "+";
 
     public Drawer() {
 
     }
 
-    public void outputTheTable(MatrixGenerator matrixGeneratorImpl) {
-        int[][] data = matrixGeneratorImpl.generateMatrix();
-        int size = matrixGeneratorImpl.getSize();
-        int minDigitCapacity = defineMinDigitCapacity(size);
-        int maxDigitCapacity = defineMaxDigitCapacity(size);
+    public String outputTheTable(MatrixGenerator matrixGenerator) {
+        var data = matrixGenerator.generateMatrix();
+        var size = data.length;
+        var biggestMatrixValue = size * size;
 
-        setFirstColumn(size);
-        setStandardColumn(size);
-        setHorizontalSeparator(size);
+        /*Определение ширины колонок в разрядах*/
+        var firstColumnWidth = getColumnWidth(size);
+        var standardColumnWidth = getColumnWidth(biggestMatrixValue);
+
+        //Инициализация строкового представления разделителей для первой и стандартной колонок, а так же общего
+        //горизонтального разделителя таблицы
+        var firstColumnUnderline = getColumnUnderline(firstColumnWidth);
+        var standardColumnUnderline = getColumnUnderline(standardColumnWidth);
+        var horizontalSeparator = getHorizontalSeparator(firstColumnUnderline, standardColumnUnderline, size);
+
+        var outputLineBuilder = new StringBuilder();
+
+        /*Отрисовка самой первой пустой ячейки таблицы*/
+        outputLineBuilder.append(String.format("%" + (firstColumnWidth) + "s", " "));
+        outputLineBuilder.append(VERTICAL_BORDER);
+
+        drawIndexRow(data, standardColumnWidth, horizontalSeparator, outputLineBuilder);
 
         for (int i = 0; i < data.length; i++) {
+            /*отрисовка левого индексного столбца*/
+            outputLineBuilder.append(String.format("%" + firstColumnWidth + "d", i + 1));
+            outputLineBuilder.append(VERTICAL_BORDER);
+
+            /*Отрисовка самой таблицы умножения из заранее сгенерированной матрицы*/
             for (int j = 0; j < data.length; j++) {
-                if (i == 0 & j == 0) {
-                    System.out.printf("%" + (minDigitCapacity) + "s", " ");
-                    System.out.print(VERTICAL_SEPARATOR);
-                    continue;
-                }
+                outputLineBuilder.append(String.format("%" + standardColumnWidth + "d", data[i][j]));
 
-                if (j == 0) {
-                    System.out.printf("%" + minDigitCapacity + "d", data[i][j]);
-                } else {
-                    System.out.printf("%" + maxDigitCapacity + "d", data[i][j]);
-                }
-
-                if (j != size) {
-                    System.out.print(VERTICAL_SEPARATOR);
+                if (j != size - 1) {
+                    outputLineBuilder.append(VERTICAL_BORDER);
                 }
             }
-            System.out.print("\n");
-            System.out.printf("%" + (maxDigitCapacity + 1) + "s", horizontalSeparator);
-            System.out.print("\n");
-        }
-    }
-
-    public void setFirstColumn(int size) {
-        int temp = size;
-
-        StringBuilder sb = new StringBuilder();
-
-        while (temp > 0) {
-            sb.append(PIECE);
-            temp /= 10;
+            outputLineBuilder.append(String.format("\n%" + (standardColumnWidth + 1) + "s\n", horizontalSeparator));
         }
 
-        firstColumn = sb.toString();
+        return outputLineBuilder.toString();
     }
 
-    public void setStandardColumn(int size) {
-        int temp = size * size;
+    /*Отрисовка индексной строки*/
+    private void drawIndexRow(int[][] data, int standardColumnWidth, String horizontalSeparator, StringBuilder outputLineBuilder) {
+        for (int i = 0; i < data.length; i++) {
+            outputLineBuilder.append(String.format("%" + standardColumnWidth + "d", i + 1));
 
-        StringBuilder sb = new StringBuilder();
-
-        while (temp > 0) {
-            sb.append(PIECE);
-            temp /= 10;
-        }
-
-        standardColumn = sb.toString();
-    }
-
-    public void setHorizontalSeparator(int size) {
-        StringBuilder sb = new StringBuilder();
-
-        for (int i = 0; i < size + 1; i++) {
-            if (i == 0) {
-                sb.append(firstColumn);
+            if (i != data.length - 1) {
+                outputLineBuilder.append(VERTICAL_BORDER);
             } else {
-                sb.append(standardColumn);
+                outputLineBuilder.append(String.format("\n%" + (standardColumnWidth + 1) + "s\n", horizontalSeparator));
             }
+        }
+    }
 
-            if (i != size) {
-                sb.append(KNUCKLE);
+    /*Возвращает строковое представление горизонтального разделителя для отдельной колонки*/
+    private String getColumnUnderline(int width) {
+        return HORIZONTAL_BORDER.repeat(width);
+    }
+
+    /*Возвращает горизонтальный разделитель таблицы, состоящий из разделителя для первой колонки, разделителей стандартных
+    колонок и перекрестий*/
+    private String getHorizontalSeparator(String firstColumnUnderline, String standardColumnUnderline, int size) {
+        var sb = new StringBuilder();
+
+        sb.append(firstColumnUnderline);
+        sb.append(CROSS_BORDER);
+
+        for (int i = 0; i < size; i++) {
+            sb.append(standardColumnUnderline);
+
+            if (i < size - 1) {
+                sb.append(CROSS_BORDER);
             }
         }
 
-        horizontalSeparator = sb.toString();
+        return sb.toString();
     }
 
-    public int defineMinDigitCapacity(int size) {
-        int minDigitCapacity = 0;
-        int temp = size;
+    /*Определяет ширину колонки, необходимую для форматирования, исходя из размера переданного числа*/
+    private int getColumnWidth(int size) {
+        var width = 0;
+        var temp = size;
 
         while (temp > 0) {
-            minDigitCapacity++;
+            width++;
             temp /= 10;
         }
 
-        return minDigitCapacity;
-    }
-
-    public int defineMaxDigitCapacity(int size) {
-        int maxDigitCapacity = 0;
-        int temp = size * size;
-
-        while (temp > 0) {
-            maxDigitCapacity++;
-            temp /= 10;
-        }
-
-        return maxDigitCapacity;
+        return width;
     }
 }
