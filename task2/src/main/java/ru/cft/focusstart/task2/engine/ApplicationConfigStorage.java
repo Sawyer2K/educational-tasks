f260227538913c7710a9784558e684756b3e1c7b
Andrey Sbitnev <sbitnev.job@yandex.ru>
2021-10-30 17:25:59 +0700

    create ApplicationConfigReader class

---
+++ task2/src/main/java/ru/cft/focusstart/task2/engine/ApplicationConfigStorage.java
@@ -0,0 +1,23 @@
+package ru.cft.focusstart.task2.engine;
+
+public class ApplicationConfigStorage {
+
+    private static String pathToInputFile;
+    private static String pathToOutputFile;
+
+    public String getPathToInputFile() {
+        return pathToInputFile;
+    }
+
+    public void setPathToInputFile(String s) {
+        pathToInputFile = s;
+    }
+
+    public String getPathToOutputFile() {
+        return pathToOutputFile;
+    }
+
+    public void setPathToOutputFile(String s) {
+        pathToOutputFile = s;
+    }
+}
