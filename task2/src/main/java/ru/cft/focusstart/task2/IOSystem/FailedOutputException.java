8ee818458888cc660b146f7ef84c6e620de6ebf8
Andrey Sbitnev <sbitnev.job@yandex.ru>
2021-11-15 14:35:03 +0700

    create FailedOutputException class

---
+++ task2/src/main/java/ru/cft/focusstart/task2/IOSystem/FailedOutputException.java
@@ -0,0 +1,8 @@
+package ru.cft.focusstart.task2.IOSystem;
+
+public class FailedOutputException extends RuntimeException {
+
+    public FailedOutputException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
